547ed99bd0f24135f02fa3639422179bcc74297f
Jeffrey <wangjh@chinacscs.com>
2021-09-02 17:39:02 +0800

    first commit

---
+++ src/components/articleUpload/ArticleUpload.tsx
@@ -0,0 +1,18 @@
+/**
+ * Created by hao.cheng on 2017/5/8.
+ */
+ import React from 'react';
+//  import { Row, Col, Card, Switch } from 'antd';
+import BreadcrumbCustom from '../BreadcrumbCustom';
+ 
+ class ArticleUpload extends React.Component {
+     render() {
+         return (
+             <div>
+                <BreadcrumbCustom first="动画" second="基础动画" />
+             </div>
+         )
+     }
+ }
+ 
+ export default ArticleUpload;
